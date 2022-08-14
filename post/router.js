1ff1829658ed4cb9d99288e2b77913f44d7d58c8
Snehasish Roy <105574399+snehasishxd@users.noreply.github.com>
2022-08-14 13:18:34 +0530

    Post requests Router

---
+++ post/router.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const _regEx = /(?:\.([^.]+))?$/;
+
+const router = (app) => {
+    fs.readdirSync(__dirname).forEach((file) => {
+        const file_name = file.substring(0, file.lastIndexOf('.'));
+        const file_extension = _regEx.exec(file);
+        if(file_name == 'router' && file == 'router.js') return;
+        const $data = require(`./${file_name}`);
+        app.post(`${$data.route}`, (req, res) => {
+            $data.exec(req, res);
+        });
+    });
+}
+
+module.exports = router;
