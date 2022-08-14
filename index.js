745d00534c5ada84e8ad31e38016aeb9e6fe2a9f
Snehasish Roy <105574399+snehasishxd@users.noreply.github.com>
2022-08-14 13:17:21 +0530

    Main File

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+var bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const expressip = require('express-ip');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const error = require('./middleware/error');
+
+app.use(logger('dev'));
+app.use(express.json()); 
+app.use(error);
+app.use(express.static("./public"));
+app.set('views', path.join(__dirname, 'pages'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(expressip().getIpInfoMiddleware);
+app.use(cookieParser());
+app.set("json spaces", 1);
+
+mongoose.connect(process.env.MONGODB_URI, () => { console.log('[DB] | Database: Connected') });
+
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    res.append('X-Frame-Options', 'SAMEORIGIN');
+    next();
+});
+
+require('./get/router')(app);
+require('./post/router')(app);
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {console.log(`[^] Web Server Launched: http://localhost:${PORT}`)});
+
